698a7db3d12640dd0f4e9d1e3d35e429c4371ffb
Anirban Ghosh <anirban23rd@hotmail.com>
2017-01-21 17:27:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function displayOutput() {
+            var input1 = document.getElementById("name").value;
+            if (input1.length === 0) {
+                alert("Please enter a valid input");
+                return;
+            }
+			
+			var input2 = document.getElementById("name").value;
+            if (input2.length === 0) {
+                alert("Please enter a valid input");
+                return;
+            }
+			
+           document.getElementById("result").innerHTML = " Username : " + input1 + "<br>" + " Password   : " + input2;
+}
